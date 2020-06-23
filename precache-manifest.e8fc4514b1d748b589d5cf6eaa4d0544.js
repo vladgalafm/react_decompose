2b1ea5afaebf240ca3af2fd9f1c5e43bd98a5e79
Vladyslav Halushkin <v.halushkin@gmail.com>
2020-06-23 23:33:40 +0300

    Updates

---
+++ precache-manifest.e8fc4514b1d748b589d5cf6eaa4d0544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7405efe17a8f9a403d46ac08a69d1d32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d08c09b397b15e08759",
+    "url": "./static/css/main.6ffafda5.chunk.css"
+  },
+  {
+    "revision": "b0dff7fd6bb18487afb8",
+    "url": "./static/js/2.0ce2c40f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ce2c40f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d08c09b397b15e08759",
+    "url": "./static/js/main.c3527986.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
